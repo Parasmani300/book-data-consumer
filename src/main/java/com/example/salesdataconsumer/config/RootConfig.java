0010436cd53db5fc7084b56c5a5a6d72857a6d90
paras <parasmani300@gmail.com>
2023-04-14 10:11:04 +0530

    Adding the remote kafka config

--- src/main/java/com/example/salesdataconsumer/config/RootConfig.java
+++ src/main/java/com/example/salesdataconsumer/config/RootConfig.java
@@ -1,7 +1,9 @@
 package com.example.salesdataconsumer.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -20,8 +22,30 @@ public class RootConfig {
     public ConsumerFactory<String,String> consumerFactory()
     {
         Map<String,Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+
+        String bootstrapServer = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092";
+        String saslmechansim = "PLAIN";
+        String jaasConfig = "org.apache.kafka.common.security.plain.PlainLoginModule required username='O2FP6ZCGYM5NVB4J' password='A9LK6mev3hEsKo/sj8zK6AdkSWnsRUs9fS8dAlkdcCtw8U0wyDqtJwQ2kgJyV71u';";
+        String securityProtocol = "SASL_SSL";
+        String sessionTimeout = "45000";
+
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG,GROUP_ID);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
         props.put(ConsumerConfig.GROUP_ID_CONFIG,GROUP_ID);
+//        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,autoCommitValue);
+//        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,kafkaConsumerEnableAutoCommit);
+//        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,kafkaSessionTimeOut);
+//        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG,heartbeatInterval);
+//        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG,heartbeatInterval);
+//        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,maxPollRecords);
+//        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,maxPoolInterval);
+//        if(activeProfile.equals("prod")) {
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, jaasConfig);
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG,GROUP_ID);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,JsonDeserializer.class);
 
--- src/main/java/com/example/salesdataconsumer/kafka/SalesConsumer.java
+++ src/main/java/com/example/salesdataconsumer/kafka/SalesConsumer.java
@@ -17,29 +17,29 @@ import java.util.Map;
 
 @Component
 public class SalesConsumer {
-    @KafkaListener(topics = "BOOK-STORE-SAMPLE",groupId = "sales-101")
+    @KafkaListener(topics = "BOOK-STORE-SAMPLE",groupId = "sales-101",containerFactory = "concurrentKafkaListenerContainerFactory")
     public void consumeMessage(Object message) throws IOException {
         ConsumerRecord<String,String> consumerRecord = (ConsumerRecord<String, String>) message;
-        postHttpRequestWithJSON(consumerRecord.value());
+//        postHttpRequestWithJSON(consumerRecord.value());
         System.out.println(consumerRecord.value());
     }
 
-    public void postHttpRequestWithJSON(String jsonRecord) throws IOException {
-        String jsonRecord1 = "";
-        if(jsonRecord != null && !jsonRecord.startsWith("[")){
-            jsonRecord1 = "[" + jsonRecord + "]";
-        }
-
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://localhost:8983/solr/sample_core/update");
-        StringEntity entity = new StringEntity(jsonRecord1);
-        httpPost.setEntity(entity);
-        httpPost.setHeader("Accept", "application/json");
-        httpPost.setHeader("Content-type", "application/json");
-        CloseableHttpResponse response = client.execute(httpPost);
-
-        System.out.println("Published the message");
-        client.close();
-    }
+//    public void postHttpRequestWithJSON(String jsonRecord) throws IOException {
+//        String jsonRecord1 = "";
+//        if(jsonRecord != null && !jsonRecord.startsWith("[")){
+//            jsonRecord1 = "[" + jsonRecord + "]";
+//        }
+//
+//        CloseableHttpClient client = HttpClients.createDefault();
+//        HttpPost httpPost = new HttpPost("http://localhost:8983/solr/sample_core/update");
+//        StringEntity entity = new StringEntity(jsonRecord1);
+//        httpPost.setEntity(entity);
+//        httpPost.setHeader("Accept", "application/json");
+//        httpPost.setHeader("Content-type", "application/json");
+//        CloseableHttpResponse response = client.execute(httpPost);
+//
+//        System.out.println("Published the message");
+//        client.close();
+//    }
 
 }
